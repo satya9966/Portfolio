0c4aae4ea0c692ee7f246a4d986c507154a4faa7
satya9966 <61901563+satya9966@users.noreply.github.com>
2022-05-07 08:31:01 +0530

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -38,21 +38,7 @@ export default class Projects extends Component {
               
               
             </div>
-          </div>
-        </section>
-<section className="colorlib-work" data-section="projects">
-          <div className="colorlib-narrow-content">
-            <div className="row">
-              <div
-                className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box"
-                data-animate-effect="fadeInLeft"
-              >
-                <h2 className="colorlib-heading animate-box">
-                  Recent Projects
-                </h2>
-              </div>
-            </div>
-            <div className="row">
+<div className="row">
               <div
                 className="col-md-5 animate-box"
                 data-animate-effect="fadeInLeft"
@@ -76,6 +62,7 @@ export default class Projects extends Component {
             </div>
           </div>
         </section>
+
       </div>
     );
   }
